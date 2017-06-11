5ea9d50d8a102cc0116cbea523f17385f1edf86b
Stephen Hand <steve2727@hotmail.com>
2017-06-11 22:44:49 +0100

    Added support for Internet Explorer via the ActiveX API (tested on IE9)

--- Native/Xslt.js
+++ Native/Xslt.js
@@ -4,21 +4,45 @@
 
 
 var _stephenhand$elm_xslt$Native_Xslt = function(){
-    var isSupported = (window && (window.XSLTProcessor || window.ActiveXObject || "ActiveXObject" in window));
+    var XSLT_API;
+    if (window && window.XSLTProcessor){
+        XSLT_API = "MOZILLA";
+    }
+    else if (window && (window.ActiveXObject || "ActiveXObject" in window)){
+        XSLT_API = "IE_ACTIVEX";
+    }
+    else{
+        XSLT_API = "UNSUPPORTED";
+    }
 
     var parser = new DOMParser();
     var serializer = new XMLSerializer();
 
+    function createTransformer(stylesheet){
+        var xslDoc, xmlDoc;
+        if (XSLT_API==="MOZILLA"){
+            xslDoc = parser.parseFromString(stylesheet, "text/xml");
+            var processor = new XSLTProcessor();
+            processor.importStylesheet(xslDoc);
+            return function(xml){
+                xmlDoc = parser.parseFromString(xml, "application/xml")
+                return serializer.serializeToString(processor.transformToDocument(xmlDoc));
+            }
+        }
+        else if (XSLT_API==="IE_ACTIVEX"){
+            xslDoc =new ActiveXObject("Msxml2.DOMDocument.3.0");
+            xslDoc.loadXML(stylesheet);
+            return function(xml){
+                xmlDoc =new ActiveXObject("Msxml2.DOMDocument.3.0");
+                xmlDoc.loadXML(xml);
+                return xmlDoc.transformNode(xslDoc);
+            }
+        }
+        else return void(0);
+    }
+
     return {
         transform : function(stylesheet){
-            if (!isSupported){
-                return function(source){
-                    _elm_lang$core$Result$Err({
-                        message:"XSLT not supported.",
-                        code:1
-                    });
-                }
-            }
             if (!stylesheet){
                 return function(source){
                     return _elm_lang$core$Result$Err({
@@ -27,18 +51,25 @@ var _stephenhand$elm_xslt$Native_Xslt = function(){
                     });
                 }
             }
-            var processor =  new XSLTProcessor();
+            var transformer;
             try{
-                var xslDoc = parser.parseFromString(stylesheet, "text/xml");
-                processor.importStylesheet(xslDoc);
+                transformer = createTransformer(stylesheet);
             }
             catch(e){
-                 return function(source){
-                     return _elm_lang$core$Result$Err({
-                         message:"Error loading stylesheet:\r\n"+e.toString(),
-                         code:3
-                     });
-                 }
+                return function(source){
+                    return _elm_lang$core$Result$Err({
+                        message:"Error loading stylesheet:\r\n"+e.toString(),
+                        code:3
+                    });
+                }
+            }
+            if (!transformer){
+                return function(source){
+                    _elm_lang$core$Result$Err({
+                        message:"XSLT not supported.",
+                        code:1
+                    });
+                };
             }
             return function(source){
                 if (!source){
@@ -47,20 +78,8 @@ var _stephenhand$elm_xslt$Native_Xslt = function(){
                         code:4
                     });
                 }
-                var xmlDoc;
                 try{
-                    xmlDoc = parser.parseFromString(source, "application/xml");
-                }
-                catch(e){
-                    return _elm_lang$core$Result$Err({
-                        message:"Error deserializing source data:\r\n"+e.toString(),
-                        code:5
-                    });
-                }
-                try{
-
-                    var outputDoc = processor.transformToDocument(xmlDoc);
-                    return _elm_lang$core$Result$Ok(serializer.serializeToString(outputDoc));
+                    return _elm_lang$core$Result$Ok(transformer(source));
                 }
                 catch(e){
                     return _elm_lang$core$Result$Err({
@@ -69,9 +88,6 @@ var _stephenhand$elm_xslt$Native_Xslt = function(){
                     });
                 }
             }
-        },
-        transformSource : function(source, transformer){
-            return transformer(source);
         }
     }
 }();
